bad7f263035917a9431cabbb6bbd3e1dceb81e84
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-23 08:35:45 +0100

    Kotlin Project

---
+++ test/AirConditionerTest.kt
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+
+class AirConditionerTest {
+
+
+   @Test
+   fun powerOffAc_AcIsOffTest(){
+      val airConditioner = AirConditioner()
+      airConditioner.powerOff()
+      assertFalse(airConditioner.isOn())
+   }
+
+   @Test
+   fun powerOnAc_AcIsOnTest(){
+      val airConditioner = AirConditioner()
+      airConditioner.powerOn()
+      assertTrue(airConditioner.isOn())
+   }
+
+   @Test
+   fun powerOnAc_PowerOffAc_acIsOffTest(){
+      val airConditioner = AirConditioner()
+      airConditioner.powerOn()
+      airConditioner.powerOff()
+      assertFalse(airConditioner.isOn())
+   }
+
+   @Test
+   fun powerOnAc_increaseTemperature_temperatureIsIncreasedTest(){
+      val airConditioner = AirConditioner()
+      airConditioner.powerOn()
+      airConditioner.increaseTemperature(1)
+      assertEquals(airConditioner.getTemperature(), 17)
+   }
+
+}
