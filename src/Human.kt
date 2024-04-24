ccc67eaf08ed88276ce21e63c98a47faf129b5f2
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-24 09:02:19 +0100

    Kotlin Project

---
+++ src/Human.kt
@@ -0,0 +1,11 @@
+class Human(name: String, age: Int) {
+    val theName: String = name
+    val theAge: Int = age
+}
+
+fun main(args: Array<String>) {
+    val person = Human("Jane Doe", 30)
+
+    val person2 = Person("Jane Doe", 30)
+
+}
